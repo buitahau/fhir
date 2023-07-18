0b9da8aacd3525a40b2ae619f1c2d4b3034cefd7
owt-hau <buitahau@gmail.com>
2023-07-18 16:57:32 +0700

    [Apache Camel] Itegrate camel with actimemq

--- src/main/java/owt/training/fhir/route/TimedJobs.java
+++ src/main/java/owt/training/fhir/route/TimedJobs.java
@@ -3,11 +3,28 @@ package owt.training.fhir.route;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class TimedJobs extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+//        timerEx1();
+        timerSendToQueue();
+        timerReadFromQueue();
+    }
+
+    private void timerReadFromQueue() {
+        from("jms:queue:HELLO.WORLD")
+                .log("Received a message - ${body}");
+    }
+
+    private void timerSendToQueue() {
+        from("timer:mytimer?period=5000")
+                .setBody(constant("HELLO from Camel!"))
+                .to("jms:queue:HELLO.WORLD");
+    }
+
+    private void timerEx1() {
         from("timer:search-all-patients?delay={{application.camel.delay}}&period={{application.camel.period}}")
                 .routeId("search-all-patients")
                 .bean("patientServiceImpl", "findAll()")
